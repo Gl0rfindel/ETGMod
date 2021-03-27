5bfb9210461ab721920b6403c71f6eca5d184480
Gl0rfindel <G6FIloiRVh@protonmail.com>
2021-03-27 15:49:31 -0700

    optimize StringDB/StringDBTable
    Keep a dictionary instead of a list that is rescanned
    on every update. This effectively eliminates up to 200ms
    spent in the setter during startup.

--- Assembly-CSharp.Base.mm/src/Core/Databases/StringDB.cs
+++ Assembly-CSharp.Base.mm/src/Core/Databases/StringDB.cs
@@ -33,60 +33,48 @@ public sealed class StringDB {
 
 public sealed class StringDBTable {
 
-    internal StringDBTable(Func<Dictionary<string, StringTableManager.StringCollection>> _getTable) {
-        _GetTable = _getTable;
+    private readonly Func<Dictionary<string, StringTableManager.StringCollection>> _getTable;
+    private readonly Dictionary<string, StringTableManager.StringCollection> _changes;
+    private Dictionary<string, StringTableManager.StringCollection> _cachedTable;
+
+    internal StringDBTable(Func<Dictionary<string, StringTableManager.StringCollection>> getTable) {
+        _getTable = getTable;
+        _changes = new Dictionary<string, StringTableManager.StringCollection>();
     }
 
-    private readonly Func<Dictionary<string, StringTableManager.StringCollection>> _GetTable;
-    private Dictionary<string, StringTableManager.StringCollection> _CachedTable;
     public Dictionary<string, StringTableManager.StringCollection> Table {
         get {
-            return _CachedTable ?? (_CachedTable = _GetTable());
+            return _cachedTable ?? (_cachedTable = _getTable());
         }
     }
 
-    private readonly List<string> _ChangeKeys = new List<string>();
-    private readonly List<StringTableManager.StringCollection> _ChangeValues = new List<StringTableManager.StringCollection>();
-
     public StringTableManager.StringCollection this[string key] {
-        get {
-            return Table[key];
-        }
-        set {
+        get => Table[key];
+        set 
+        {
             Table[key] = value;
-
-            int i = _ChangeKeys.IndexOf(key);
-            if (i > 0) {
-                _ChangeValues[i] = value;
-            } else {
-                _ChangeKeys.Add(key);
-                _ChangeValues.Add(value);
-            }
-
+            _changes[key] = value;
             JournalEntry.ReloadDataSemaphore++;
         }
     }
 
-    public bool ContainsKey(string key) {
-        return Table.ContainsKey(key);
-    }
+    public bool ContainsKey(string key) => Table.ContainsKey(key);
 
     public void Set(string key, string value) {
-        StringTableManager.StringCollection value_ = new StringTableManager.SimpleStringCollection();
-        value_.AddString(value, 1f);
-        this[key] = value_;
+        var collection = new StringTableManager.SimpleStringCollection();
+        collection.AddString(value, 1f);
+        this[key] = collection;
     }
 
-    public string Get(string key) {
-        return StringTableManager.GetString(key);
-    }
+    public string Get(string key) => StringTableManager.GetString(key);
 
     public void LanguageChanged() {
-        _CachedTable = null;
+        _cachedTable = null;
         Dictionary<string, StringTableManager.StringCollection> table = Table;
-        for (int i = 0; i < _ChangeKeys.Count; i++) {
-            table[_ChangeKeys[i]] = _ChangeValues[i];
+
+        foreach (var kvp in _changes)
+        {
+            table[kvp.Key] = kvp.Value;
         }
     }
-
 }
